1d5f82b4ad44865f276cda73b0e3b0f407462877
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-08 16:38:04 +0000

    Added first printf code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*printf - prints formated string to standard output
+*Return  0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is piece of art,");
+	return (0)
+}

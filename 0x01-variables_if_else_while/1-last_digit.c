7ff36f452bd864b1985520476273dc2a8b9de498
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-08 20:56:52 +0000

    Added last digit checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,13 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	if ((n % 10) < 5)
-	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	}
 	if ((n % 10) == 0)
 	{
-	printf("Last digit of %d is %d and is zero\n", n, n % 10);
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	if ((n % 10) != 0 && (n % 10) < 6 )
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }

b1afbbb007f58bff92d413ccbe889b8bc2482878
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-08 17:26:40 +0000

    Added sizeof programe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
 */
 int main(void)
 {
-	printf("Size of char: %d\n", sizeof(char));
-	printf("Size of an int: %d\n", sizeof(int));
-	printf("Size of a long int: %d\n", sizeof(long int));
-	printf("Size of long long int: %d\n", sizeof(long long int));
-	printf("Size of a float: %d\n", sizeof(float));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }

38f6c0229a8f509fb0dba32e5590a960e55c6356
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-08 20:38:43 +0000

    Added last digit checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	b = n % 10;
-	if (b > 5)
+	if ((n % 10) > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, b);
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	if (b < 5)
+	if ((n % 10) < 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, b);
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	if (b == 0)
+	if ((n % 10) == 0)
 	{
-	printf("Last digit of %d is %d and is zero\n", n, b);
+	printf("Last digit of %d is %d and is zero\n", n, n % 10);
 	}
 	return (0);
 }

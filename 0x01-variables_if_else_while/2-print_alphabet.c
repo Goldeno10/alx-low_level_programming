7b8edb931000b06de80948c0ad798bdd2f7609ad
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-08 21:11:16 +0000

    print a-z with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*putchar: print characters to the standard out
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha = "abcdefghijklmnopqrstuvwxyz"
+	for (i in alpha)
+	{
+	putchar(i);
+	}
+	return (0)
+}

463daa8373981fe4cc94122ae98360a64da400e7
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-09 13:08:49 +0000

    Added task 9

---
+++ 0x01-variables_if_else_while/9-print_com.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main- Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(0120;
+	return (0);
+}

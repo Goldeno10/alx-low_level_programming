9ff39a230bf41d7196575709e723b120b610b83a
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-09 11:27:42 +0000

    Added a-z and A-Z printer

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*putchar : used to print characters  to the standard output
+*
+*Return : Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i="a";i<="z";i++)
+	{
+		putchar(i);
+	}
+	for (x="A";x<="Z";x++)
+	{
+		putchar(x);
+	}
+	putchar(012);
+	return (0)
+}

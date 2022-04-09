c13b67f7cf5eedb93232bd03baa28f1b1a46fae4
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-09 12:35:04 +0000

    added task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*mai- Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 48; x = 57; x++)
+	{
+		putchar(x);
+	}
+	putchar(012);
+	return (0)
+}

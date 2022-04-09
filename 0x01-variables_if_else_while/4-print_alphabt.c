6b35c82d1cd42dce1bedee5964fc3026d547da94
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-09 11:47:13 +0000

    Added task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main- Entry point
+*
+*putchar: used to print characters to standard output
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 145  || x != 161)
+		{
+			puthchar(x);
+		}
+	}
+	putchar(012);
+	return (0);
+}

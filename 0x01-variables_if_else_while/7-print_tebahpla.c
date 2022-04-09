e5a7c9cd5f8bd52a42fac278cdbc433efe6ac98c
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-09 12:43:29 +0000

    added task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main- Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 'z' , x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar(012);
+	return (0);
+}

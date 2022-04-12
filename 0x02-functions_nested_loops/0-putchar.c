824871ddbfbef9324b140b72ba924a644ffd4d49
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-12 15:09:00 +0000

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,15 @@
 */
 int main(void)
 {
-	puts("_putchar\n");
-	return (0);
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }

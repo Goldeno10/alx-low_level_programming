363ce37c2d880c735f61697dc9f3dad216bb362f
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-12 15:39:02 +0000

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar(012);
+}
+

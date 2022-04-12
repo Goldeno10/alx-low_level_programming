1752291ee5fdce1632fe5f60caf5e03b646c6275
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-12 12:13:26 +0000

    added task 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,5 +8,5 @@
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59)
-return (1)
+return (1);
 }
--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -23,6 +23,5 @@ int main(void)
 		}
 	}
 	putchar(012);
-	return (0);
 }
 

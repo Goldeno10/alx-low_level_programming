d785631680494b01be02ef2bbd4309400ad72ad5
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-12 15:58:57 +0000

    Commit message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,5 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-
-
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
 
 void print_alphabet(void)
 {

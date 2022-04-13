774a43c66a12a58b724caa82ba24bb83d9ce76e5
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 13:32:32 +0000

    added task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		_putchar(43);
 		return (1);
 	}
 	if (n == 0)
 	{
-		putchar (48);
+		_putchar (48);
 		return (0);
 	}
 	if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		return (-1);
 	}
 }

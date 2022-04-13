e60c1bba912a58e3ed3656b830a050facbe63cc1
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 12:50:07 +0000

    added task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower: check if  a character is lower case or not
+*
+*Return: 1 if true and  0 if false
+*/
+
+int _islower(int c)
+{
+	if (c = 'a'; c <= 'z'; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+}

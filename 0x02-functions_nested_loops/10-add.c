299f8afc4fce075d9c2cda165f1ac4fd7989592d
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 20:58:21 +0000

    Added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add- Add two integers
+*@a: Holds the first integer
+*@b: Holds the second integer
+*
+*Return: Return sum of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}

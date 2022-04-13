9e1f91990b949fa38767a145635e88489e20a460
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 19:11:06 +0000

    added task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*times_table- prints the 9 time table
+*
+*Return: Reeturns nothing
+*/
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int c = a * b;
+
+			_putchar(c + '0');
+			_puthchar(44);
+			_putchar(20);
+		}
+	_putchar(012);
+	}
+}
+

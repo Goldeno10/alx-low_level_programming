b765c72d81c2a2244a632ab4a4ff7f1002c71158
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 13:10:04 +0000

    added task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,9 @@
 /**
 *_isalpha- Checks if a character is an alphabet
 *
-*@c : Hold the character being compared
+*@c: Hold the character being compared
 *
-*Return : Alawys 1 if true and 0 otherwise
+*Return: Alawys 1 if true and 0 otherwise
 */
 int _isalpha(int c)
 {

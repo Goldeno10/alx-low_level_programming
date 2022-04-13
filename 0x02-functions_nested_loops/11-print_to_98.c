ac641b8ddfd0c420a33a49d6d221d87cc77e9c8a
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 21:54:09 +0000

    Added task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,53 @@ void print_to_98(int n)
 	int a = n / 10;
 	int b = n % 10;
 	int c;
-
-	for (c = n; c <= 98; c++)
+	
+	if (n <= 98)
 	{
-		if (n < 9)
+		for (c = n; c <= 98; c++)
 		{
-			_putchar(n + '0');
-		}
-		else
-		{
-			_putchar(a + '0');
-			_putchar(b + '0');
-		}
-		if (n != 98)
-		{
-		_putchar(',');
-		_putchar(' ');
-		}
-		else
-		{
-			_putchar(0);
+			if (n < 9)
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar(a + '0');
+				_putchar(b + '0');
+			}
+			if (n != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
+			{
+				_putchar(0);
+			}
 		}
 	}
+	else
+	
+		for (c = n; c <= 98; c--)
+		{
+			if (n < 9)
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar(a + '0');
+				_putchar(b + '0');
+			}
+			if (n != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
+			{
+				_putchar(0);
+			}
+		}
+
 }

417a86960047c9f67df44fdfc6b72eff1c6ab27d
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 13:24:18 +0000

    added task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_sign- Checks if a number is greater, equal or less than zero
+*
+*@n: holds the value being compared
+*
+*Return: 1 to n > 0, 0 for n = 0 and -1 for n < 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		putchar(43);
+	}
+	if (n = 0)
+	{
+		return (0);
+		putchar (48);
+	}
+	if (n < 0)
+	{
+		return (-1);
+		putchar(45)
+	}
+}

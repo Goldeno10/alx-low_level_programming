23a6f8567b5fbc1599129ba308284a330f1a5672
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 18:34:45 +0000

    added task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*jack_bauer- prints time jack bauer movie esq
+*
+*@a: holds and count the hour
+*@b: holdes and count the minute
+*
+*Return: Returns nothing
+*/
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar(012);
+
+		}
+	}

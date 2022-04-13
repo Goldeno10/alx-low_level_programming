4eef23760fcbda32d972511ba2dfadadcc88c63f
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 20:31:12 +0000

    Added task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,10 +22,6 @@ void times_table(void)
 				{
 					_putchar(44);
 				}
-				else
-				{
-					_putchar(0);
-				}
 				_putchar(0);
 				_putchar(32);
 			}
@@ -33,14 +29,10 @@ void times_table(void)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				
+
 				if (b != 9)
 				{
 					_putchar(44);
-				}
-				else
-				{
-					_putchar(0);
 				}
 				_putchar(32);
 			}

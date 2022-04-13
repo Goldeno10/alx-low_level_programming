f251cde1ac9451d9e3c68071ea8bcd895c47f760
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 13:04:30 +0000

    added task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+/**
+*_isalpha - Checks if a character is an alphabet
+*
+*@c : Holde the character being coimared
+*Return : i if true and 0 otherwise
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

3d707b4e33478b7c3fe2603ec3c12789b754738b
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-13 12:56:42 +0000

    added task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_islower: check if  a character is lower case or not
+*_islower - check if  a character is lower case or not
 *@c: holds tha parameter being compared
 *Return: 1 if true and  0 if false
 */

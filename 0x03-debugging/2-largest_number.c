e0d93b13cb46a993b9595e255d764c0e6e56cc80
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-14 17:40:45 +0000

    task 0

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,19 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-<<<<<<< HEAD
-	if (a > b && (b > c || b = c))
-	{
-		largest = a;
-	}
-	else if (b > a && (a > c || a = c))
-=======
 	if (a > b && (b > c || b == c))
 	{
 		largest = a;
 	}
 	else if (b > a && (a > c || a == c))
->>>>>>> 3923dce7a6cab9feddd49953e0e7ec5925107121
 	{
 		largest = b;
 	}

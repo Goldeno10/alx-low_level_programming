1b1510bb8ac3e9c04429df657ad795bd8e6a2f45
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-15 15:06:31 +0000

    Added task 3 0f debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (((year % 400 == 0) && (year % 100 == 0)) || (year % 4 == 0))
+	if (((year % 400 == 0) && !(year % 100 == 0)) || (year % 4 == 0))
 	{
 		if (month == 2 && day >= 60)
 		{
@@ -31,17 +31,6 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
-	while (d > 9)
-	{
-	d /= 10;
-	count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-	_putchar(((m / count) % 10) + 48);
-	}
+		}	}
 }
 

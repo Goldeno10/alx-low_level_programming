85462670502e08c77211e8316f36fa1a6a96a4d8
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-16 13:55:45 +0000

    added task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_most_numbers- prints numbers 0 through  9
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 50 || c != 52)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar(012);
+}

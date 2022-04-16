1c473d5a9dbc57186b8d30db9cfcdbf9968812ce
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-16 12:50:48 +0000

    Added task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*mul- multilies two integers
+*
+*Return: Always the multiple of the two input
+*/
+int mul((int a, int b)
+{
+	return ( a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 void _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);

ca067b9840634c8ddeddbf2f8138152846d37fac
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-16 13:05:15 +0000

    Added task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers- prints numbers 0 through  9
+*
+*Return: returns nothing
+*/
+void print_numbers(void);
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+		_putchar(012);
+}

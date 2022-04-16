0ac74fde7855c905ca5ca328c71f2b3585d1d71b
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-16 14:29:28 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*print_numbers- prints numbers 0 through  9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+	int a;
+	int b;
+	int c;
+
+	i = 0;
+	while (i < 10)
+	{
+		while (c < 14)
+		{
+			if (c < 10)
+			{
+				a = c;
+			}
+			else
+			{
+				b = c;
+			}
+			_putchar(a);
+			_putchar(b);
+		}
+		i++;
+	}
+	_putchar(012);
+}

08dc26337f68b175bd7e2ac9098706451f5443c9
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-16 14:56:45 +0000

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,32 +6,31 @@
 */
 void print_numbers(void)
 {
-int a;
-int b;
-int c;
-int i;
+	int a;
+	int b;
+	int c;
+	int i;
 
-i = 0;
-while (i < 10)
-{
-while (c < 14)
-{
-if (c < 10)
-{
-b = c;
-}
-else
-{
-a = c / 10;
-b = c % 10;
-}
-_putchar(a + '0');
-_putchar(b + '0');
-
-c++;
-}
-i++;
-c = 0;
-}
-_putchar(012);
+	i = 0;
+	while (i < 10)
+	{
+		while (c < 14)
+		{
+			if (c < 10)
+			}
+				b = c;
+			}
+			else
+			{
+				a = c / 10;
+				b = c % 10;
+			}
+			_putchar(a + '0');
+			_putchar(b + '0');
+			c++;
+		}
+		i++;
+		c = 0;
+	}
+	_putchar(012);
 }

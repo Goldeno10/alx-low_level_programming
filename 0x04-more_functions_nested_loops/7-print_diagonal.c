f94563271aa54176503577a539e5b0dbbb7b22f6
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-16 16:43:28 +0000

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_line- print dashes n time
+*@n: hols the value being counted
+*Return: returns nothing
+*/
+void print_diagonal_line(int n)
+{
+	int b;
+	int c;
+
+	c = 0;
+	while ( c < n)
+	{
+		for (b = 0; b < n; b++)
+		{
+			_putchar('0');
+		}
+	}
+	c++;
+	_putchar('\n');
+}

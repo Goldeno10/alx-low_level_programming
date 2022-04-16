b3f58671c73aed47f1990949d87f2476144e9950
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-16 15:42:37 +0000

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_line- print dashes n time
+*@n: hols the value being counted
+*Return: returns nothing
+*/
+void print_line(int n)
+{
+	int c;
+
+	c=0;
+	if (n > 0)
+	{
+		while (c <= n)
+		{
+			_putchar('-');
+                	c++;
+		}
+		 _putchar(012);
+        }
+	else
+	{
+		_putchar(012);
+	}
+}

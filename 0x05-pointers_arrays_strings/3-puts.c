07857de3f2ed6a7804087373bf9c3a523a9cabb7
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-21 20:13:03 +0000

    added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_puts- prints a string followed by a new line
+*@str: Holdes the string 
+*Return: return nothing
+*/
+void _puts(char *str)
+{
+	puts("%s\n",str);
+}

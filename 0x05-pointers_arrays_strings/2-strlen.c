9524cd8ca7eab57dd7f7bdfaab4e4f9e3eadfcc7
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-21 19:55:08 +0000

    added task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 /**
+*_strlen- returns the lenght of a string
 *
-*
-*
+*@s: holds the  string
+*Return: always 0 (success)
 */
 int _strlen(char *s);
 {
 	strlen(s);
+	return (0);
 }

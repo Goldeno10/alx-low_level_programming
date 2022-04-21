04c1ec900ef0b129db8bdc1ae0f3de7b2c1dfc20
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-21 19:13:31 +0000

    Added task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*swap_int- Swaps the value of two integers using pointers
+*@a,@b : hold the parameters being changed
+*
+*Return: Return nothing
+*/
+void swap_int(int *A, int *b)
+{
+	*a = b;
+	*b = a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,4 @@
 void _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+

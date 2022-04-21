f43fcefe8503da13cc72da7cbb44c92b60a73928
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-21 18:56:35 +0000

    task 0 of 5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reset_to_98 -takes a pointer to an int as parameter and updates the value it points to
+*
+*Return: Returns nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+void _putchar(char c);
+void reset_to_98(int *n);

267524c81be0de2d0e9c59ca6be863381fe53100
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-22 18:15:53 +0000

    Added task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*rev_string- reverses astring.
+*
+*@s: holds the string being manipulated.
+*Return: Returns nothing.
+*/
+void rev_string(char *s)
+{
+	int c;
+	int len;
+	char temp
+
+	len = 0;
+	while (s[len])
+	{
+		len++;
+	}
+
+	for (c = 0; c < len / 2; c++)
+	{
+		temp = s[c];
+		s[c] = *(s +(len-1));
+		*(s + (len - 1)) = temp;
+	}
+}

399359e9dea217d81d9196c3c2a93b1109df5be9
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-23 16:02:10 +0000

    Added task 2

---
+++ 0x06-pointers_arrays_strings/2-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*_strncpy- copy n number two strings.
+*
+*@dest: holds the first string pointer
+*@src: holds the secind string string pointer.
+*@n: Holds number of byte to concated
+*Return: Returns pointer.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int len_dest;
+	int len_src;
+
+	len_dest = 0;
+	while (dest[len_dest] != '\0')
+	{
+		len_dest++;
+	}
+	len_src = 0;
+	while (src[len_src] != '\0' && len_src < n)
+	{
+		dest[len_src] = src[len_src];
+		len_src++;
+	}
+
+	dest[len_src] = '\0';
+	return (dest);
+}

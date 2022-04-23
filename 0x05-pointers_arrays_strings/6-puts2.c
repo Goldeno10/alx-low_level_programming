5c6823dc1c4f523cfe7ecf64d8a44bf73946db85
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-23 00:24:27 +0000

    Added task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,13 +12,13 @@ void puts2(char *str)
 	int len;
 
 	len = 0;
-	while (s[len] != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
 	}
 	for (c = 0; c <= (len - 1); c += 2)
 	{
-		_putchar(s[c]);
+		_putchar(str[c]);
 	}
 	_putchar(012);
 }

7bd67f7ddb757f263617cd8c72d882694d679263
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-23 02:52:08 +0000

    Added task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int c;
 
-	for (c = 0; c != '\0'; c++)
+	c = 0
+	while (src[c] != '\0')
 	{
 		dest[c] = src[c];
+		c++;
 	}
 	dest[c] = '\0';
 	return (dest);

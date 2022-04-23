5b25b21b76207e0ffb42e5fda562b3e4a42d03a1
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-23 02:13:22 +0000

    Added task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_strcpy- print half the string.
+*
+*@dest: holds the buffer pointer
+*@src: holds the cours string pointer.
+*Return: Returns pointer.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int c;
+	int len;
+	char temp;
+
+	len = 0;
+	while (str[len] <= '\0')
+	{
+		len++;
+	}
+	n = len 
+	for (c = 0; c <= len; c++)
+	{
+		dest[c] = scr[c];
+	return (*dest);
+}

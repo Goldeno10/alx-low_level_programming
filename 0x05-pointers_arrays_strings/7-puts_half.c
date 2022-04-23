2a86dcd6926afce27ac21addd6bfac2d123eb27a
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-04-23 00:39:46 +0000

    Added task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*puts_half- print half the string.
+*
+*@str: holds the string being manipulated.
+*Return: Returns nothing.
+*/
+void puts_half(char *str)
+{
+	int c;
+	int len;
+	int n
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	n = len - 1;
+	for (c = 0; c <= n; c++)
+	{
+		if (len % 2 == 0)
+		{
+			_putchar(str[(c + (len / 2)]);
+		}
+		else
+		{
+			_putchar(str[c + (n / 2)];
+		}
+	}
+	_putchar(012);
+}

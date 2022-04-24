0b7eca92eeaef0e229eb2e5740b9b63258ea5d62
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-04-24 14:34:29 +0100

    modified task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,9 @@ int _strcmp(char *s1, char *s2)
 	ans  = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		ans += (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+		{
+			ans = (s1[i] - s2[i]);
 		i++;
 	}
 	return (ans);

ff78f4487e2137b23a79b357935af2a81abd5007
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-04-27 12:53:53 +0100

    task 0 modified

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
-*_memset - fills 1st n bytes of the memory pnted to by s with the const byte b
+*_memset- fill fst n bytes of memory pnted to by s with the const byte b
 *
 *@s: pointer to a string
 *@b: The constant byte to be used for replacemnt
 *@n: number of bytes
 *
-*Return: Always a pointer
+*Return: A  pointer
 */
 char *_memset(char *s, char b, unsigned int n);
 {
 	unsigned int i;
 
 	i = 0;
-	while (i <= n)
+	while (i <  n)
 	{
 		*(s + i) = b;
 		i++;

df0716207559686a0d13d15d6f1fc12c5eb9dd29
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-04-27 11:43:51 +0100

    Added task 0 of part 7

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_memset- fills the first n bytes of the memory area pointed to by s with the constant byte b
+*
+*@s: pointer to a string
+*@b: The constant byte to be used for replacemnt
+*@n: number of bytes
+*
+*Return: Always a pointer
+*/
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}

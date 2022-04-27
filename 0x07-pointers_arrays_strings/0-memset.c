d824658ff26056af24ea627ccf9486c4370b0af1
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-04-27 12:57:40 +0100

    task 0 modified

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,10 +13,9 @@ char *_memset(char *s, char b, unsigned int n);
 	unsigned int i;
 
 	i = 0;
-	while (i <  n)
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
-		i++;
 	}
-	return (s);
+	return(s);
 }

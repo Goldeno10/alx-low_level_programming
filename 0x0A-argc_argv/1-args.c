9c447e2cadf5dd5459415847e9bc0c88b4b58c11
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-04 11:45:26 +0100

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Main entry point
+*@argc: Holds number if arguments
+*@argv: Holds the arguments
+*Return: Always 0 (Success)
+*/
+
+
+int main(int argc, char *argv[])
+{
+        (void) argv;
+
+        printf("%d\n", argc);
+        return (0);
+}

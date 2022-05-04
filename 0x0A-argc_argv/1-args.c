c7eb13d195832574905d53e2df0a0fe4ac6d0e9b
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-04 12:34:58 +0100

    task 0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-        (void) argv;
+	(void) argv;
 
-        printf("%i\n", (argc - 1));
+	printf("%i\n", (argc - 1));
 
-        return (0);
+	return (0);
 }

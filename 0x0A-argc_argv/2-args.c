164664eab0b0ef234a3cd0174bc7dee1e761a4a8
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-04 12:58:50 +0100

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,10 +11,14 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	{
 
+	printf ("%s\n", argv[0]);
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
 	return (0);
 }

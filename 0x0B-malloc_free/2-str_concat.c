a6741965a5382746e5ae5c817daadb2b35f6f174
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-05 20:47:43 +0100

    ltask 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -53,32 +53,34 @@ int _strlen(char *s)
 */
 char *str_concat(char *s1, char *s2)
 {
-
-	int t_len = _strlen(s1) + _strlen(s2) + 1;
-	int i;
-
-	unsigned int len_s1, len_s2;
 	char *str;
+	unsigned int i, c, size;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-	_strcat(s1, s2);
-	len_s1 = _strlen(s1);
-	len_s2 = _strlen(s2);
-	t_len =  (len_s1 + len_s2 + 1);
-	str = malloc(sizeof(char) * t_len);
-	if (str == NULL)
+	}
+	size = (_strlen(s1) + _strlen(s2) + 1);
+
+	str = (char *) malloc(size * sizeof(char));
+
+	if (str == 0)
 	{
 		return (NULL);
 	}
 
-	 _strcat(s1, s2);
-	for (i = 0; i < t_len; i++)
+	for (i = 0; *(s1 + i) != '\0'; i++)
 	{
-		str[i] = s1[i];
+		*(str + i) = *(s1 + i);
 	}
-	str[i] = '\n';
+	for (c = 0; *(s2 + c) != '\0'; c++)
+	{
+		*(str + i) = *(s2 + c);
+	}
+
 	return (str);
 }

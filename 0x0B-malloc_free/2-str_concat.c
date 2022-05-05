76996734c857ea182e3a22a532b3a8fdf8548499
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-05 15:26:04 +0100

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,72 @@
+#include "main.h"
+/**
+*_strcat- concatinate two strings.
+*
+*@dest: holds the first string pointer
+*@src: holds the secind string string pointer.
+*Return: Returns pointer.
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len_dest;
+	int len_src;
+
+	len_dest = 0;
+	while (dest[len_dest] != '\0')
+	{
+		len_dest++;
+	}
+	len_src = 0;
+	while (src[len_src] != '\0')
+	{
+		dest[len_dest + len_src] = src[len_src];
+		len_src++;
+	}
+	dest[len_dest + len_src] = '\0';
+	return (dest);
+}
+#include "main.h"
+#include <stdlib.h>                               /**
+*_strlen - evaluate the lenght of a string
+*@s: Pointer to the string
+*Return: An inteteger(length of string)
+*/
+
+unsigned int _strlen(char *s)
+{                                                         unsigned int len;
+	unsigned int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int t_len = _strlen(s1) + _strlen(s2) + 1;
+	unsigned int i;
+	char *cp_str;
+	char *str = malloc(t_len * sizeof(char));
+
+	*str = _strcat(s1, s2);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	*cp_str = _strcat(s1, s2);
+	for ( i = 0; i < t_len; i++)
+	{
+		str[i] = cp_str[i];
+	}
+	str[i] = '\n';
+	return (str);
+}

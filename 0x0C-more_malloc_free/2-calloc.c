59ed70215b8397e55a5c041d9e7b9937defc555b
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-09 20:55:25 +0000

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_calloc - Allocate memory to an array
+*@nmemb: Memory size of array elements
+*@size: Number of array elements
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned int *arr = malloc(nmemb * size);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	if ((nmemb || size) == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+
+}

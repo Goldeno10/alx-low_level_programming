6c4ae56e96ecb9c8bc5196adc38c59d697b63f83
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-09 18:23:48 +0100

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
 /**
 *malloc_checked - Allocates a memory using malloc
 *@b: Size of memory needed
@@ -7,7 +7,7 @@
 */
 void *malloc_checked(unsigned int b)
 {
-	unsigned int *mem = malloc(sizeof(*mem) * b);
+	int *mem = malloc(sizeof(*mem) * b);
 	if (mem == NULL)
 		exit (98);
 

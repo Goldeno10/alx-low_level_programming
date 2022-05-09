3c950e52c2eb375357e40a0aa5e42b557aa07a57
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-09 20:25:20 +0000

    task 0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@ unsigned int _strlen(char *s)
 	unsigned int len;
 
 	len = 0;
-	while (s[len]!= 0)
+	while (s[len] != 0)
 	{
 		len++;
 	}
@@ -22,6 +22,7 @@ unsigned int _strlen(char *s)
 *string_nconcat - Concatenates string in n byte
 *@s1: String 1
 *@s2: String 2
+*@n: Number of bytes
 *Return: Pointer to a string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -29,8 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int len_s1 = _strlen(s1);
 	unsigned int len_s2 = _strlen(s2);
-	/*unsigned int _n = (n > len_s2)? len_s2: n;*/
-	char *str = malloc((sizeof(char)* len_s1) + n);
+	unsigned int _n = (n >= len_s2) ? len_s2 : n;
+	char *str = malloc((sizeof(char) * len_s1) + _n);
 
 	if (str == NULL)
 	{
@@ -40,30 +41,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n < len_s2)
-	{
-		for (i = 0; i <= len_s1; i++)
-		{
-			str[i] = s1[i];
-		}
 
-		for (i = 0; i < n; i++)
-		{
-			str[len_s1 + i] = s2[i];
-		}
-	}
-	else if (n >= len_s2)
+	for (i = 0; i <= len_s1; i++)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			str[i] = s1[i];
-		}
-
-		for (i = 0; i <= len_s2; i++)
-		{
-			str[len_s1 + i] = s2[i];
-		}
+		str[i] = s1[i];
 	}
+
+	for (i = 0; i < _n; i++)
+	{
+		str[len_s1 + i] = s2[i];
+	}
+
 	str[len_s1 + i] = '\0';
 
 	return (str);

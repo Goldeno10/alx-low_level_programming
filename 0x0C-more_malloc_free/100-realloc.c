444d05dc29e840c3aefd0c86d737d29aaf827fec
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-09 23:00:17 +0000

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,26 +13,32 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (ptr != NULL)
+	{
 		copy = ptr;
+	}
 	else
 	{
-		 return (malloc(new_size));
+		return (malloc(new_size));
 	}
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
-		 free(ptr);
+		free(ptr);
 		return (0);
 	}
 
 	relloc = malloc(new_size);
 	if (relloc == NULL)
+	{
 		return (0);
+	}
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
 		*(relloc + i) = copy[i];
 	}
 	free(ptr);
-return (relloc);
+	return (relloc);
 }

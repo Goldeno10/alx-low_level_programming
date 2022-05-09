5ab46de0ea144c919f73056e08dcec42f9b44c05
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-09 20:04:13 +0100

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_strlen - Evaluate the lenght of string
+*@s: Pointerto the string
+*Return: integer
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s != 0)
+	{
+		len++;
+	}
+	return (len)
+}
+
+
+/**
+*string_nconcat - Concatenates string in n byte
+*@s1: String 1
+*@s2: String 2
+*Return: Pointer to a string
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i;
+	int len_s1 = _strlen(s1);
+	int len_s2 = _strlen(s2);
+	char *str = malloc(sizeof(char)* len_s1 + n);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n < len_s1)
+	{
+		for (i = 0; i <= len_s1; i++)
+		{
+			str[i] = s1[i];
+		}
+
+		for (i = 0; i < n; i++)
+		{
+			str[len_s1 + i] = s2[i];
+		}
+	}
+	else if (n >= len_s2)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			str[i] = s1[i];
+		}
+
+		for (i = 0; i <= len_s2; i++)
+		{
+			str[len_s1 + i] = s2[i];
+		}
+	}
+	str[len_s1 + i] = '\0';
+
+	return (str);
+}

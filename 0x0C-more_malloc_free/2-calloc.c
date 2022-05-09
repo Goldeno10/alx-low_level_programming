d2c42e7b156eca909e21744b26eb42ab0d45f37a
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-09 21:09:21 +0000

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *arr = malloc(nmemb * size);
 
-	if ((nmemb || size) == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}

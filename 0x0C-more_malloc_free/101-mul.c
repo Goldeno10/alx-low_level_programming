1d566409e953e8096ef2f99b49e0679f8d07e7d1
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-09 23:05:50 +0000

    task 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: Arguments count
+ * @argv: argument s
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i;
+	int c;
+	if (argc != 3)
+	{ 
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (c = 0; argv[i][c] != '\0'; c++)
+		{
+			if (argv[i][c] > 57 || argv[i][c] < 48)
+			{
+				printf("Error\n");
+				exit(98); }
+		}
+
+	}
+	mul = atol(argv[1]) *atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
+}

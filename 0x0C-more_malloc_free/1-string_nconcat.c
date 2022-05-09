7cf0fe754e9a704d625e398c5fb2ef7ba0a49b28
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-09 22:29:33 +0000

    Task 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,12 +11,10 @@ unsigned int _strlen(char *s)
 
 	len = 0;
 	while (s[len] != 0)
-	{
 		len++;
-	}
+
 	return (len);
 }
-
 
 /**
 *string_nconcat - Concatenates string in n byte
@@ -50,7 +48,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 	}
 
-	for (i = 0; i < _n; i++)
+	for (i = 0; i <= _n; i++)
 	{
 		str[len_s1 + i] = s2[i];
 	}

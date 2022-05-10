0c45299373b72edf7b9e1966a54bf8ca1fbd20e6
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-10 00:06:21 +0000

    task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,30 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 /**
  * main - multiplies two positive numbers
- * @argc: n arguments
+ * @argc: Arguments count
  * @argv: arguments vector
- * Return: int
+ * Return: integer
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	int mul, a, b;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
 	}
-	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+
+	if (!(isdigit(argv[1]) || isdigit(argv[2])))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a *b;
+	printf("%d\n", mul);
+	return (0);
 }

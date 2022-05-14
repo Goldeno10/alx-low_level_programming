513959a52528b8fe97507b1bdc1636757c7ba6f4
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-14 16:47:33 +0100

    Modified task 3

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,14 @@
 *@name: name to be printed
 *@f: function pointer to do the printing
 *Return: Nothing
+*parses the name to a function pointed to
+* by the function pointer
 */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if(name && f)
+	{
 		f(name);
+	}
 }

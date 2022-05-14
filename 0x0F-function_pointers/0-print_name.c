eb40d605699f6a60a2ced9705c6c169a3366fb14
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-14 15:15:04 +0100

    task 0 to 2 modified

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,12 @@
 #include "function_pointers.h"
+
 /**
 *print_name - prints name
 *@name: name to be printed
 *@f: function pointer to do the printing
 *Return: Nothing
+*prints the name using function pointer
+*only when name and print function is provided
 */
 
 void print_name(char *name, void (*f)(char *))

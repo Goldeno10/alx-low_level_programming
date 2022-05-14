499705a49805746bc4758e8e0c97317b65c18844
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-14 16:05:47 +0100

    Modified task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,11 +23,12 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (s == ops[i].op)
+		if (strcmp(s, ops[i].op) == 0)
 		{
 			return (ops[i].f);
 		}
 		i++;
 	}
+	printf("Error\n");
 	exit(99);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,13 +14,16 @@ int main(int argc, char *argv[])
 	int x;
 
 	if (argc != 4)
+	{
+		printf("Error\n");
 		exit(98);
-
+	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	c = argv[2];
 
-	x = (*get_op_func(c))(a, b);
+	x = get_op_func(c)(a, b);
 	printf("%d\n", x);
+
 	return (0);
 }

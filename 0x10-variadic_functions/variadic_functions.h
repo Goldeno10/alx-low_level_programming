02c8e7c83b7bc8dab0421a5ef2801663043a2ecc
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-16 13:09:52 +0000

    header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef V_FUNCTIONS_H
+#define V_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+void _putchar(char c);
+void print_all(const char * const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif

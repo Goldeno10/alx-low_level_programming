8b6b274c3be84ecbecd340cfa777dea90e4ff812
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-16 02:38:40 +0000

    task 0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		(d->name) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-		printf("Age: (nil)\n");
+		(printf("Age: %f\n", d->age)
 		(d->owner) ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 	}
 

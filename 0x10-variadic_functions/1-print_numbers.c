d5aa9162e7a8f9af89f74a8b2fe437bd6e6a6b6b
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-16 15:17:33 +0100

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,21 +1,20 @@
 #include "variadic_functions.h"
 /**
-*
-*
-*
-*
+*print_numbers- rint inputed numbers with a seperator
+*@seperator: seperates the numbers
+*@n: nubee of digits
+*Retirn: Nothing
 */
 void print_numbers(const char *seperator, const unsigned int n, ...)
 {
 	va_list nums;
-	int i;
+	unsigned int i;
 
-	if (seperator)
-	{
-		for (i = 0; i < n; i = va_arg(nums, unsigned int)
-			printf("%d%s ", i, seperator);
-	}
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+		(seperator) ? printf("%d%s ", va_arg(nums, unsigned int), seperator) : printf("%d ", va_arg(nums, unsigned int));
 
+	va_end(nums);
 
-
+	putchar('\n');
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,11 @@
 #ifndef V_FUNCTIONS_H
 #define V_FUNCTIONS_H
+void _putchar(char c);
 
 #include <stdio.h>
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
-void _putchar(char c);
 void print_all(const char * const format, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

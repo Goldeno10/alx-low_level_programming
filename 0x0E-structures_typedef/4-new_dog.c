1acd50345fcc37357afc508c3ac0e9f4df2695a0
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-16 02:19:29 +0000

    task 0

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,14 +11,37 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int i = 0, j = 0, c;
 
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
+		free(dog);
 		return (NULL);
-
-	dog->name = name;
+	}
+	dog->name = malloc(i * sizeof(dog->name));
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (c = 0; c <= i; c++)
+		dog->name[c] = name[c];
 	dog->age = age;
-	dog->owner = owner;
-
+	dog->owner = malloc(j * sizeof(dog->owner));
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (c = 0; c <= j; c++)
+		dog->owner[c] = owner[c];
 	return (dog);
 }

6ee635593a3fe9b972c8c6a0eb001ef8a343b366
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-24 16:25:11 +0100

    updated repo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,10 +8,13 @@
 
 size_t print_list(const list_t *h)
 {
+	int num_of_node = 0;
+
 	while (h != NULL)
 	{
 		(h->str) ? printf("%s", h->str) : printf("[0] (nill)");
 		h = h->next;
-	)
-	return (o);
+		num_of_node++;
+	}
+	return (num_of_node);
 }

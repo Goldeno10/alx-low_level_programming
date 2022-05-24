d055963eb115b166dadc03a023a3687d81aa5d7e
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-05-24 19:28:25 +0000

    updated repo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		(h->str != NULL) ? printf("[%d]%s\n", h->len,  h->str) : printf("[0] (nill)\n");
+		(h->str != NULL) ? printf("[%lu]%s\n", h->len,  h->str) : printf("[0] (nill)\n");
 		h = h->next;
 		num_of_node++;
 	}
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,7 +7,7 @@
 */
 size_t list_len(const list_t *h)
 {
-	size_t len;
+	size_t len = 0;
 
 
 	while (h != NULL)

9c474b1526737a75369f980c45b6cc30470b208d
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-25 15:36:34 +0100

    added task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = len;
 	new_node->next = NULL;
 
-	if ( *head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);

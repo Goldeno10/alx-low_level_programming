a0c3331dcdc79159a08817ddf792069915971e0b
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-25 15:30:56 +0100

    added task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+*add_node_end - adds a new node at the end of a list_t list.
+*@head: Pointer to first lust element
+*@str: string data
+*Return: the address of the new element, or NULL if it failed
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int len = 0;
+	list_t *new_node;
+	list_t *last = *head;
+
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = NULL;
+
+	if ( *head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,9 +20,15 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+
 size_t print_list(const list_t *h);
+
 size_t list_len(const list_t *h);
+
 int _putchar(char c);
+
 list_t *add_node(list_t **head, const char *str);
+
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* #define LIST_H */

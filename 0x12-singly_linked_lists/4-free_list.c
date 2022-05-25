6d5ff1e3775f41b52ab8e7397e6503ae908ee416
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-25 16:02:55 +0100

    added task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,18 +1,22 @@
 #include "lists.h"
 /**
-*
-*
-*
+*free_list - Frees a
+*@head: head point of a list
+*Return:  Nothing
 */
 void free_list(list_t *head)
 {
+	list_t *temp = NULL;
+
 	if (head == NULL)
 	 return;
 
 	while (head->next != NULL)
 	{
+		temp = head->next;
 		free(head->str);
-		head = head->next;
+		free(head)
+		head = temp;
+
 	}
-	free(head);
 }

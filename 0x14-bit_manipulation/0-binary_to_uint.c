9a31b4c8c9346eeb61333473310ae320fb73df6f
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-05-30 22:42:23 +0100

    Bulk commits

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+*binary_to_unit - Converts binary to units
+*b: binary bits
+*Return: unit value of binary bits
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num, x;
+	int len;
+
+	if (!b)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+		;
+	len--;
+	for (x = 1, num = 0; len >= 0; len--)
+	{
+		if (b[len] == '0')
+		{
+			x *= 2;
+			continue;
+		}
+		else if (b[len] == '1')
+		{
+			num += x;
+			x *= 2;
+			continue;
+		}
+		return (0);
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <math.h>
+
+unsigned int _pow(int n, unsigned int e);
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+
+#endif /* #endif MAIN_H */

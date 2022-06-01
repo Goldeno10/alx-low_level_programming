ee0a13957345e204839438605c7eb0e237b6fe4f
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-06-01 13:29:03 +0100

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#define PERM 0600
+/**
+*creat_file - creates a file and write to it
+*@filename: Filename
+*@text_content: Text content)
+*Return: 1 (succes) or -1 (failiure)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[len])
+		len++;
+
+	if ((fd = creat(filename, PERM)) == -1)
+		return (-1);
+	if ((n = write(fd, text_content, len)) != len)
+		return (-1);
+	return (1);
+}

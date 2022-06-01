86ffcfd60ca7d489bab7ef8d83eed36eb1f48fc4
Muhammad Ibrahim Baba <ibrahimmuhammad271@gmail.com>
2022-06-01 21:09:32 +0000

    updated task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,9 +13,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	while (text_content[len])
 		len++;
-	fd = open(filename, O_RDWR | O_APPEND);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)

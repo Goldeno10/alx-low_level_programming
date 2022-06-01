45b8caf0e964a1f02b78126bd2f6265f44fe0343
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-06-01 12:50:58 +0100

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,11 +10,12 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-		return (0);
-
 	int fd, n, x = 0;
-	char *buf= malloc(sizeof(size_t));
+	char *buf;
+
+	if (filename == NULL)
+                return (0);
+	buf = malloc(sizeof(size_t));
 
 	if ((fd = open(filename, O_RDONLY)) == -1)
 		return (0);

6282999d8030543112406a0edca7788be4e90434
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-12 21:46:12 +0000

    ..

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -12,7 +12,7 @@ int _strlen(char *s)
 	int len = 0;
 
 	while (s[len] != '\0')
-		len ++;
+		len++;
 
 	return (len);
 }
--- 0x18-dynamic_libraries/4-isalpha.c
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_islower - check if  a character is an alphabet 
+*_isalpha - check if  a character is an alphabet
 *@c: holds tha parameter being compared
 *Return: 1 if true and  0 if false
 */

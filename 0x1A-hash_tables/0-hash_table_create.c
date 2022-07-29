8403987242e50f7ac5d3f50f4038f2f9833a3cca
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-29 21:31:11 +0000

    ...

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table with a given
- * size
+ * hash_table_create - creates a hash table with a given size
  * @size: size of the hash table
- * Return: the created hash table, or NULL if function
- * fails
+ * Return: the created hash table, or NULL if function fails
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -33,11 +33,11 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (i == last_index)
 				{
-					printf("'%s': '%s'", (char *)node->key, (char *)node->value);
+					printf("'%s': '%s'", node->key, node->value);
 				}
 				else
 				{
-					printf("'%s': '%s', ", (char *)node->key, (char *)node->value);
+					printf("'%s': '%s', ", node->key, node->value);
 				}
 				node = node->next;
 			}

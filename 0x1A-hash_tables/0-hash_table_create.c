5273483b3f64874a68a8fd236a325b2df294335a
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-29 22:01:27 +0000

    ...

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
+	array = malloc(sizeof(hash_node_t) * size);
 	if (array == NULL)
 		return (NULL);
 

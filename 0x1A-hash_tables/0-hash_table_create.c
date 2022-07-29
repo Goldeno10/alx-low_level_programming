39fb21963b50a2c110446850c0d3853ff12c7ae7
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-29 22:04:28 +0000

    final

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t *));
 	if (table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t) * size);
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
 		return (NULL);
 

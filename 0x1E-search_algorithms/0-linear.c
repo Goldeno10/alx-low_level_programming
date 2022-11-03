dc7385cc99fb1f438ea2a473882f9d40168cab68
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-11-03 13:56:34 +0000

    fixed betty requirments

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,12 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers using the 
- * Linear search algorithm
+ * linear_search - searches for a value in an array of integers using the
+ *	Linear search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: the number of elements in array
  * @value: the value to search for
+ * Return: Index of the first occurence of value or -1
  *
  */
 int linear_search(int *array, size_t size, int value)
@@ -16,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 
 	while (index < size)
-	{	
+	{
 		printf("Value checked array[%li] = [%i]\n", index, array[index]);
 		if (array[index] == value)
 			return (index);
